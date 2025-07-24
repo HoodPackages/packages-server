afc2700d0fdc0fd7fc51475b8bf5d73bf364205e
Yaroslav <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2025-07-24 12:58:35 +0300

    more inputs and values was added

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,3 +19,37 @@ exports.createProduct = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+// PUT /api/products/:id
+exports.updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // вернуть обновлённый документ
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: 'Продукт не найден' });
+    }
+
+    res.json(updatedProduct);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// DELETE /api/products/:id
+exports.deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: 'Продукт не найден' });
+    }
+
+    res.json({ message: 'Продукт удалён' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {
   getAllProducts,
-  createProduct
+  createProduct,
+  updateProduct,
+  deleteProduct
 } = require('../controllers/productController');
 
 router.get('/', getAllProducts);
 router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
 
 module.exports = router;
